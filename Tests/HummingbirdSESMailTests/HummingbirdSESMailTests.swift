bb40d58ec552624a75e9d392b513558e1619cbb1
Tibor Bodecs <mail.tib@gmail.com>
2023-05-11 18:34:12 +0200

    add mail suffix

--- Tests/HummingbirdSESMailTests/HummingbirdSESMailTests.swift
+++ Tests/HummingbirdSESMailTests/HummingbirdSESMailTests.swift
@@ -2,41 +2,57 @@ import XCTest
 import NIO
 import Hummingbird
 import HummingbirdMail
-import HummingbirdSMTP
+import HummingbirdAWS
+import HummingbirdSESMail
+import SotoCore
 import Logging
 
-final class HummingbirdMTPTests: XCTestCase {
+final class HummingbirdSESMailTests: XCTestCase {
     
     var from: String { ProcessInfo.processInfo.environment["MAIL_FROM"]! }
     var to: String { ProcessInfo.processInfo.environment["MAIL_TO"]! }
     
     private func send(_ email: HBMail) async throws {
         let env = ProcessInfo.processInfo.environment
-
+        
+        var logger = Logger(label: "aws-logger")
+        logger.logLevel = .info
+        
         let app = HBApplication()
-        app.services.setUpSMTPMailer(
-            eventLoopGroup: app.eventLoopGroup,
-            hostname: env["SMTP_HOST"]!,
-            signInMethod: .credentials(
-                username: env["SMTP_USER"]!,
-                password: env["SMTP_PASS"]!
-            )
+        app.services.aws = .init(
+            credentialProvider: .static(
+                accessKeyId: env["SES_ID"]!,
+                secretAccessKey: env["SES_SECRET"]!
+            ),
+            options: .init(
+                requestLogLevel: .info,
+                errorLogLevel: .info
+            ),
+            httpClientProvider: .createNewWithEventLoopGroup(
+                app.eventLoopGroup
+            ),
+            logger: logger
         )
 
+        app.services.setUpSESMailer(
+            using: app.aws,
+            region: env["SES_REGION"]!
+        )
+        
         try await app.mailer.send(email)
         try app.shutdownApplication()
     }
     
     // MARK: - test cases
-
+    
     func testSimpleText() async throws {
         let email = try HBMail(
             from: HBMailAddress(from),
             to: [
                 HBMailAddress(to),
             ],
-            subject: "test SMTP with simple text",
-            body: "This is a simple text email body with SMTP."
+            subject: "test ses with simple text",
+            body: "This is a simple text email body with SES."
         )
         try await send(email)
     }
@@ -47,8 +63,8 @@ final class HummingbirdMTPTests: XCTestCase {
             to: [
                 HBMailAddress(to),
             ],
-            subject: "test SMTP with HTML text",
-            body: "This is a <b>HTML text</b> email body with SMTP.",
+            subject: "test ses with HTML text",
+            body: "This is a <b>HTML text</b> email body with SES.",
             isHtml: true
         )
         try await send(email)
@@ -77,8 +93,8 @@ final class HummingbirdMTPTests: XCTestCase {
             to: [
                 HBMailAddress(to),
             ],
-            subject: "test SMTP with attachment",
-            body: "This is an email body and attachment with SMTP.",
+            subject: "test ses with attachment",
+            body: "This is an email body and attachment with SES.",
             attachments: [attachment]
         )
         try await send(email)
